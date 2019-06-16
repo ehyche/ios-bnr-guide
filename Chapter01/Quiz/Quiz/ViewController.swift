e0229a56e2e7b73217e1e1b8c5006f52ab37593f
Eric Hyche <ehyche@groupon.com>
2019-06-15 23:01:40 -0400

    Add finished Quiz application from Chapter 1 and Resources directory

---
+++ Chapter01/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Eric Hyche on 6/15/19.
+//  Copyright © 2019 Eric Hyche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+

2014628f7d702b33603db9ca60ecbbbc2528f7ca
Eric Hyche <ehyche@groupon.com>
2019-08-11 20:36:24 -0600

    Commit finished Chapter 3 WorldTrotter

---
+++ Chapter03/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Eric Hyche on 7/1/19.
+//  Copyright © 2019 Eric Hyche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//        
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+////        view.addSubview(secondView)
+//        firstView.addSubview(secondView)
+//    }
+
+}
+
